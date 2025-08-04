70710a852966cd0ba997931240645fb49bd3dff3
chloe <cwoolwine@gmail.com>
2025-08-04 13:07:43 -0700

    idk if i like this all that much, trying another plugin

---
+++ pandora/categories.gd
@@ -0,0 +1,11 @@
+# Do not modify! Auto-generated file.
+class_name PandoraCategories
+
+
+const WORLDOBJECTS = "8.0"
+
+
+class WorldObjectsCategories:
+	const PLANTS = "10.0"
+
+
---
+++ pandora/object_ids.gd
@@ -0,0 +1,5 @@
+# Do not modify! Auto-generated file.
+class_name ObjectIds
+
+
+const POPLAR = "9.0"
