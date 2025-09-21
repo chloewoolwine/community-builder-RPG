0e9c9368918711c02d3beaafd98e9402de3a8cf5
chloe <cwoolwine@gmail.com>
2025-09-20 21:15:43 -0700

    fix physics layer thing

--- Game Logic/world/elevation_layer.gd
+++ Game Logic/world/elevation_layer.gd
@@ -15,10 +15,11 @@ class_name ElevationLayer
 @onready var till: TileMapLayer = $Till
 @onready var grass: TileMapDual = $Grass
 @onready var stone: TileMapDual = $Stone
+@onready var pollution_grass: TileMapDual = $PollutionGrass
 @onready var decor: TileMapLayer = $Decor
 @onready var water_mapper: TileMapLayer = $WaterMapper
 
-@onready var arr:Array[TileMapDual] = [base, pond, sand, grass, stone]
+@onready var arr:Array[TileMapDual] = [base, pond, sand, grass, stone, pollution_grass]
 
 var moisture_gradient_map: Image
 # real tilemap coord where gradient texture should be aligned into 
@@ -34,19 +35,18 @@ func _ready() -> void:
 func set_physics_data() -> void:
 	self.tile_set = self.tile_set.duplicate()
 	self.tile_set.set_physics_layer_collision_mask(0,0)
-	var x : int = 0
-	for layer in arr:
-		#make tileset unique- this is to set the collision layers properly
-		if (layer.tile_set == null):
-			layer.tile_set = self.tile_set
-		layer.tile_set = layer.tile_set.duplicate()
-		#clear old layer
-		layer.tile_set.set_physics_layer_collision_mask(0,0)
-		layer.tile_set.set_physics_layer_collision_layer(0, 0b1 << elevation+9)
-		layer.world_source_id = x
-		x = x + 1
+	base.tile_set.set_physics_layer_collision_mask(0,0)
+	base.tile_set.set_physics_layer_collision_layer(0, 0b1 << elevation+9)
+	
+	## CHLOE LOOK AT ME 
+	## WHEN YOU ADD A NEW LAYER. GO INTO THE ELEVATION. AND ADD IT THERE TOO. OK?
+	## OR ELSE !!!!!
+	## "world source id" is the source id in the master tileset- mapper.tres
 	## World source id doesn't matter for pond because it reads from its own map
 	pond.world_source_id = 0 
+	sand.world_source_id = 2
+	grass.world_source_id = 4
+	pollution_grass.world_source_id = 1
 
 func change_square(_data: SquareData, overall_location: Vector2i) -> void:
 	#print("modifying loaded square at runtime")
@@ -54,6 +54,7 @@ func change_square(_data: SquareData, overall_location: Vector2i) -> void:
 	stone.erase_tile(overall_location)
 	base.erase_tile(overall_location)
 	sand.erase_tile(overall_location)
+	pollution_grass.erase_cell(overall_location)
 	pond.just_erase_tile(overall_location)
 	set_square(_data, overall_location)
 		
@@ -79,7 +80,7 @@ func set_square(_data: SquareData, overall_location: Vector2i)-> void:
 				grass.fill_tile(overall_location, 0)
 				#print("filled grass")
 				if _data.pollution > 2:
-					grass.fill_tile(overall_location)
+					pollution_grass.fill_tile(overall_location)
 			SquareData.SquareType.Rock:
 				#print("filled rocj")
 				base.fill_tile(overall_location)
@@ -88,7 +89,6 @@ func set_square(_data: SquareData, overall_location: Vector2i)-> void:
 				#print("filled sand")
 				base.fill_tile(overall_location)
 				sand.fill_tile(overall_location)
-	#grass.fill_tile(overall_location)
 	if _data.object_data && _data.object_data.size() > 0:
 		var floor_type := _data.object_data[0]
 		if floor_type != null:
@@ -121,11 +121,14 @@ func delete_square(overall_location: Vector2i) -> void:
 func update_specific_pixel(overall_location: Vector2i, _data:SquareData) -> void:
 	till.set_cell(overall_location, 0, Vector2i(_data.water_saturation,0))
 
-func add_grass(overall_location: Vector2i) -> void: 
+func add_grass(overall_location: Vector2i, pollution: int) -> void: 
 	grass.fill_tile(overall_location)
+	if pollution > 2:
+		pollution_grass.fill_tile(overall_location)
 
 func remove_grass(overall_location: Vector2i) -> void: 
 	grass.erase_tile(overall_location)
+	pollution_grass.erase_tile(overall_location)
 
 func till_square(overall_location: Vector2i, square: SquareData) -> void: 
 	till.set_cell(overall_location, 0, Vector2i(square.water_saturation,0))
